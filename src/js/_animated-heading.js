dd93b3f0deb2d132f3557f856731b1da4d96f94b
realpvblo <78243008+realpvblo@users.noreply.github.com>
2023-08-11 18:24:29 +0200

    pliki

--- src/js/_animated-heading.js
+++ src/js/_animated-heading.js
@@ -1,17 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const words = document.querySelectorAll(".animated-heading .word");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const words = document.querySelectorAll(".animated-heading .word");
   
-    function animateWords() {
-      words.forEach((word, index) => {
-        setTimeout(() => {
-          word.classList.add("animating");
-          setTimeout(() => {
-            word.classList.remove("animating");
-          }, 1000);
-        }, index * 1000);
-      });
+//     function animateWords() {
+//       words.forEach((word, index) => {
+//         setTimeout(() => {
+//           word.classList.add("animating");
+//           setTimeout(() => {
+//             word.classList.remove("animating");
+//           }, 1000);
+//         }, index * 1000);
+//       });
+//     }
+  
+//     setInterval(animateWords, 3000);
+//   });
+  
+var $header = $('#header');
+var prevScroll = 0;
+
+$(window).scroll(function() {
+    var currentScroll = $(this).scrollTop();
+    
+    if (currentScroll > 10) {
+        $header.css({
+            'backdrop-filter': 'saturate(180%) blur(5px)'
+        });
+    } else {
+        $header.css({
+            'backdrop-filter': 'none'
+        });
     }
-  
-    setInterval(animateWords, 3000);
-  });
-  
+    
+    prevScroll = currentScroll;
+});
+
+$(document).ready(function() {
+  $(document).on('mousemove', function(e) {
+    $('#circularcursor').css({
+      left: e.pageX,
+      top: e.pageY
+    });
+  })
+});
